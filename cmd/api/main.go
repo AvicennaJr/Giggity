4f6ca633323b4ced2aebbede430dd9a8b5961d4f
AvicennaJr <justfuad@protonmail.com>
2023-02-28 23:35:19 +0300

    Use httprouter handler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -40,7 +40,7 @@ func main() {
 
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      mux,
+		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
