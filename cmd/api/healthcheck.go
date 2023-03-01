c2f7e23f20e48048661e4b69dea5954959d2dd27
AvicennaJr <justfuad@protonmail.com>
2023-03-01 10:29:54 +0300

    update the healthcheck response

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,13 +5,15 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	data := map[string]string{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+	data := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
 
-	err := app.writeJSON(w, http.StatusOK, envelope{"healthcheck": data}, nil)
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "There was a problem", http.StatusInternalServerError)
