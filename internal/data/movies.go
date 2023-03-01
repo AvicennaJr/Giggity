2b2e1ef21d72099779d1b67cb132a18bf7596d91
AvicennaJr <justfuad@protonmail.com>
2023-03-02 01:17:32 +0300

    display data according to sort options

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/AvicennaJr/Giggity/internal/validator"
@@ -132,7 +133,7 @@ func (m MovieModel) Delete(id int64) error {
 }
 
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
-	query := "select id, created_at, title, year, runtime, genres, version from movies where (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) or $1 = '') and (genres @> $2 or $2 = '{}') order by id"
+	query := fmt.Sprintf("select id, created_at, title, year, runtime, genres, version from movies where (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) or $1 = '') and (genres @> $2 or $2 = '{}') order by %s %s, id ASC", filters.sortColumn(), filters.sortDirection())
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
