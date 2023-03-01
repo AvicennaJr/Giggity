d4d5a8bc4091fa0a580c09ec6b3ff0f3f885b644
AvicennaJr <justfuad@protonmail.com>
2023-03-02 01:39:01 +0300

    Add get metadata information

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -132,18 +132,20 @@ func (m MovieModel) Delete(id int64) error {
 	return nil
 }
 
-func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
-	query := fmt.Sprintf("select id, created_at, title, year, runtime, genres, version from movies where (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) or $1 = '') and (genres @> $2 or $2 = '{}') order by %s %s, id ASC limit $3 offset $4", filters.sortColumn(), filters.sortDirection())
+func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, Metadata, error) {
+	query := fmt.Sprintf("select count(*) over(), id, created_at, title, year, runtime, genres, version from movies where (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) or $1 = '') and (genres @> $2 or $2 = '{}') order by %s %s, id ASC limit $3 offset $4", filters.sortColumn(), filters.sortDirection())
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres), filters.limit(), filters.offset())
 	if err != nil {
-		return nil, err
+		return nil, Metadata{}, err
 	}
 
 	defer rows.Close()
+
+	totalRecords := 0
 
 	movies := []*Movie{}
 
@@ -151,6 +153,7 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 		var movie Movie
 
 		err := rows.Scan(
+			&totalRecords,
 			&movie.ID,
 			&movie.CreatedAt,
 			&movie.Title,
@@ -161,16 +164,18 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 		)
 
 		if err != nil {
-			return nil, err
+			return nil, Metadata{}, err
 		}
 
 		movies = append(movies, &movie)
 	}
 
 	if err = rows.Err(); err != nil {
-		return nil, err
+		return nil, Metadata{}, err
 	}
 
-	return movies, nil
+	metadata := calculateMetadata(totalRecords, filters.Page, filters.PageSize)
+
+	return movies, metadata, nil
 
 }
