aaca4034b9910571ad06e176708b05ec32eee99b
AvicennaJr <justfuad@protonmail.com>
2023-03-01 20:37:13 +0300

    create movie model

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/AvicennaJr/Giggity/internal/validator"
@@ -28,4 +29,21 @@ func ValidateMovie(v *validator.Validator, movie *Movie) {
 	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
 	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
 	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
+}
+
+type MovieModel struct {
+	DB *sql.DB
+}
+
+func (m MovieModel) Insert(movie *Movie) error {
+	return nil
+}
+func (m MovieModel) Get(id int64) (*Movie, error) {
+	return nil, nil
+}
+func (m MovieModel) Update(movie *Movie) error {
+	return nil
+}
+func (m MovieModel) Delete(id int64) error {
+	return nil
 }
