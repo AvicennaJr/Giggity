6b2227e7029311fd2ddc44134a38a0b142b406a4
AvicennaJr <justfuad@protonmail.com>
2023-03-01 09:47:29 +0300

    Use json encoding

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,13 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "There was a problem", http.StatusInternalServerError)
+		return
+	}
+
+	js = append(js, '\n')
+
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(js))
+	w.Write(js)
 }
