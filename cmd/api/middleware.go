2bad2f155e4ed5dead5815db48fbc5174ae34ae9
AvicennaJr <justfuad@protonmail.com>
2023-03-03 01:31:23 +0300

    split logic between activated and authenticated middleware

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -122,13 +122,20 @@ func (app *application) authenticate(next http.Handler) http.Handler {
 	})
 }
 
-func (app *application) requireActivatedUser(next http.HandlerFunc) http.HandlerFunc {
+func (app *application) requireAuthenticatedUser(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		user := app.contextGetUser(r)
 		if user.IsAnonymous() {
 			app.authenticationRequiredResponse(w, r)
 			return
 		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) requireActivatedUser(next http.HandlerFunc) http.HandlerFunc {
+	fn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := app.contextGetUser(r)
 
 		if !user.Activated {
 			app.inactiveAccountResponse(w, r)
@@ -138,4 +145,6 @@ func (app *application) requireActivatedUser(next http.HandlerFunc) http.Handler
 		next.ServeHTTP(w, r)
 
 	})
+
+	return app.requireAuthenticatedUser(fn)
 }
