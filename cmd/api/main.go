92b7f0d78977952798e596b1e19f89f6afb3448e
AvicennaJr <justfuad@protonmail.com>
2023-03-02 15:43:18 +0300

    add smtp configs to application

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/AvicennaJr/Giggity/internal/data"
 	"github.com/AvicennaJr/Giggity/internal/jsonLog"
+	"github.com/AvicennaJr/Giggity/internal/mailer"
 	_ "github.com/lib/pq"
 )
 
@@ -28,12 +29,20 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *jsonLog.Logger
 	models data.Models
+	mailer mailer.Mailer
 }
 
 func main() {
@@ -48,6 +57,11 @@ func main() {
 	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "5e65ad15dc706e", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "c77a284d19f9a6", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Giggity <no-reply@giggity.com>", "SMTP sender")
 	flag.Parse()
 
 	logger := jsonLog.New(os.Stdout, jsonLog.LevelInfo)
@@ -65,6 +79,7 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
 
 	err = app.serve()
