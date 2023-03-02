43e985a610c1612e5a5f1fbd2b7a648f7ba98290
AvicennaJr <justfuad@protonmail.com>
2023-03-03 02:40:27 +0300

    remove credentials
    I know i know

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -61,8 +61,8 @@ func main() {
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
 	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
-	flag.StringVar(&cfg.smtp.username, "smtp-username", "5e65ad15dc706e", "SMTP username")
-	flag.StringVar(&cfg.smtp.password, "smtp-password", "c77a284d19f9a6", "SMTP password")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "yourmama", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "yourpapa", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Giggity <no-reply@giggity.com>", "SMTP sender")
 	flag.Parse()
 
